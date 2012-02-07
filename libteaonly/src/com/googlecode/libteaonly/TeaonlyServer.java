83c69944d24c5b49e644cd05e234efe406af8993
achang.zhou <achang.zhou@gmail.com>
2012-02-07 06:22:09 +0000

    Added Java based local http server with streaming and CGI support.

---
+++ libteaonly/src/com/googlecode/libteaonly/TeaonlyServer.java
@@ -0,0 +1,24 @@
+package com.googlecode.libteaonly;
+
+import com.googlecode.libteaonly.NanoHTTPD;
+
+import java.io.*;
+import java.util.*;
+import android.util.Log;
+
+public class TeaonlyServer extends NanoHTTPD
+{
+    private OnRequestListen myRequestListen = null;
+    private File homeDir;
+
+    public TeaonlyServer(int port, String wwwroot) throws IOException {
+        super(port, new File( wwwroot ).getAbsoluteFile() );
+        homeDir = new File( wwwroot);
+    }
+    
+    public Response serve( String uri, String method, Properties header, Properties parms, Properties files ) {
+    }
+
+    public void serveDone( Response r) {
+    }
+}
