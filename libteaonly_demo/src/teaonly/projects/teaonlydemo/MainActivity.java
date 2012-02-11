eb81f571428c3f2b4f08e7559cf62c289b8b6a43
achang.zhou <achang.zhou@gmail.com>
2012-02-11 04:40:45 +0000

    Support local web server.

--- libteaonly_demo/src/teaonly/projects/teaonlydemo/MainActivity.java
+++ libteaonly_demo/src/teaonly/projects/teaonlydemo/MainActivity.java
@@ -1,15 +1,28 @@
 package teaonly.projects.teaonlydemo;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    private WebView webview;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
         setContentView(R.layout.main);
+        webview = (WebView)findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(false);
+        webview.loadUrl("http://www.baidu.com");
     }
+	
+
+
 }
