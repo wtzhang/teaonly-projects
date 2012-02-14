d519ea883541dfbdd87a53bb0972166bb81f5331
achang.zhou@gmail.com <achang.zhou@gmail.com@baff9159-4c17-4706-d244-21966b7d52bd>
2012-02-14 04:16:01 +0000

    NDK is ready.

--- trunk/libteaonly/jni/cgateway.cpp
+++ trunk/libteaonly/jni/cgateway.cpp
@@ -1,12 +1,29 @@
 #include "cgateway.h"
 
-static bool parseParameters(std::string &uri, std::vector<std::string > &params) {
-	return false;
+GatewayInterface *GatewayInterface::Gateway = NULL;
+
+GatewayInterface::GatewayInterface() {
 }
 
-int GatwayInterface(std::string &file, std::string &uri) {
-	std::vector<std::string> params;
-	parseParameters(uri, params);
-	return 0;	
+GatewayInterface::~GatewayInterface() {
+}
+
+int GatewayInterface::RegisterInterface(std::string &file, Interface *itf) {
+    int ret = 1;
+    if ( interfaces[file] == NULL)
+        ret = 0;                            //0:new  1:replace 
+    interfaces[file] = (void *)itf; 
+    return ret;
+}
+
+int GatewayInterface::RunInterface(std::string &file, std::string &uri, std::string &result) {
+    int ret;
+    Interface *itf = (Interface *) interfaces[file];
+    //-1:error, >=0: return code
+    if (itf == NULL)
+        ret = -1;
+    else 
+        ret = itf->Run(file, uri, result);
+    return ret;
 }
 
--- trunk/libteaonly/jni/cgateway.h
+++ trunk/libteaonly/jni/cgateway.h
@@ -1,14 +1,34 @@
-#ifndef _CGATWAY_H_
-#define _CGATWAY_H_
+#ifndef _CGATEWAY_H_
+#define _CGATEWAY_H_
 
-#include <vector>
-#include <string>
 #include <jni.h>
 #include <android/log.h>
+#include <vector>
+#include <string>
+#include <map>
 
 #define  LOG_TAG    "TEAONLY"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)  
 
-int GatwayInterface(std::string &file, std::string &uri);
+class Interface {
+public:
+    virtual int Run(std::string &file, std::string &uri, std::string &result) = 0;
+};
+
+class GatewayInterface {
+public:
+    GatewayInterface();
+    ~GatewayInterface();
+
+public:
+    int RegisterInterface(std::string &file, Interface *itf);
+    int RunInterface(std::string &file, std::string &uri, std::string &result);
+
+private:
+    std::map<std::string, void *> interfaces;
+
+public:
+    static GatewayInterface *Gateway;
+};
 
 #endif
