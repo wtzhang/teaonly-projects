611e163bb254075c7168124d0c5f9c07177bf1e8
achang.zhou@gmail.com <achang.zhou@gmail.com@baff9159-4c17-4706-d244-21966b7d52bd>
2012-02-24 06:59:59 +0000

    Added files.

--- trunk/teaplayer/app_build.mk
+++ trunk/teaplayer/app_build.mk
@@ -1,3 +1,12 @@
 # DEPS_ += src\test.h
 
-OBJS_ += src/main.o
+OBJS_ += src/main.o\
+         src/media.o\
+         src/player.o\
+         src/access.o\
+         src/demux.o\
+         src/decode.o\
+         src/vout.o\
+         src/aout.o
+
+
