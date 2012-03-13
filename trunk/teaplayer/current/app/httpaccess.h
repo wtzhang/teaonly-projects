3e14e287e878117967d6b28e1ba7069fb3da07e3
achang.zhou@gmail.com <achang.zhou@gmail.com@baff9159-4c17-4706-d244-21966b7d52bd>
2012-03-13 07:47:12 +0000

    Added http access.

---
+++ trunk/teaplayer/current/app/httpaccess.h
@@ -0,0 +1,40 @@
+#ifndef _HTTPACCESS_H_
+#define _HTTPACCESS_H_
+
+#include "access.h"
+#include "talk/base/thread.h" 
+#include "talk/base/messagequeue.h"
+
+namespace talk_base{
+    class AsyncSocket;
+};
+
+class HttpAccess : public TeaAccess, talk_base::MessageHandler{ 
+public:
+    HttpAccess();
+    virtual ~HttpAccess();
+
+    virtual bool Open();
+    virtual void Close();
+
+protected:
+    void OnMessage(talk_base::Message *msg);
+    void OnConnectEvent(talk_base::AsyncSocket* socket);
+    void OnCloseEvent(talk_base::AsyncSocket* socket, int err);
+    void OnReadEvent(talk_base::AsyncSocket* socket);
+
+private:
+    enum {
+        MSG_ACCESS_OPEN,
+    };
+    void httpQuery();
+    void doConnect();
+    void createSocket();
+
+private:
+    talk_base::Thread *thread;
+    talk_base::AsyncSocket* http_socket_;
+    bool is_ready_;
+};
+
+#endif
