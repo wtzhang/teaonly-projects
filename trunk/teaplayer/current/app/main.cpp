9a28c100e0c1f5b5531f4b2bbb296815c1b68d8b
achang.zhou@gmail.com <achang.zhou@gmail.com@baff9159-4c17-4706-d244-21966b7d52bd>
2012-03-13 07:46:50 +0000

    Clean code and backup.

--- trunk/teaplayer/current/app/main.cpp
+++ trunk/teaplayer/current/app/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 #include "fileaccess.h"
+#include "httpaccess.h"
 #include "ffdemux.h"
 #include "vout.h"
 #include "player.h"
 
 int main(int argc, const char *argv[]) {
 
-    FileAccess *access = new FileAccess( argv[1] );    
+    //FileAccess *access = new FileAccess( argv[1] );    
+    HttpAccess *access = new HttpAccess();
+
     FFDemux *demux = new FFDemux("live.flv");
     TeaVideoOutput *vout = new TeaVideoOutput();
     TeaPlayer *player = new TeaPlayer(access, demux, vout);
