30f2a9e796963e1260214abde53d35735382b134
achang.zhou@gmail.com <achang.zhou@gmail.com@baff9159-4c17-4706-d244-21966b7d52bd>
2012-03-19 05:23:47 +0000

    Added Android support.

---
+++ trunk/teaplayer/jni/app/main_jni.cpp
@@ -0,0 +1,68 @@
+#include <time.h>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include "fileaccess.h"
+#include "httpaccess.h"
+#include "ffdemux.h"
+#include "bmp_vout.h"
+#include "player.h"
+
+#define  JNIDEFINE(fname) Java_tv_ppcam_broidviewer_LiveVideo_##fname
+
+//static HttpAccess *access;
+static FileAccess *access;
+static FFDemux *demux;
+//static TeaVideoOutput *vout;
+static BmpVideoOutput *vout;
+static TeaPlayer *player;
+
+extern "C" {
+  JNIEXPORT jint JNICALL JNIDEFINE(startPlayer)(JNIEnv* env, jobject obj, jobject bmp_fb);
+  JNIEXPORT jint JNICALL JNIDEFINE(renderBMP)(JNIEnv* env, jobject obj, jobject bmp_fb);
+  JNIEXPORT jint JNICALL JNIDEFINE(stopPlayer)(JNIEnv* env, jclass clz);
+};
+
+
+JNIEXPORT jint JNICALL JNIDEFINE(startPlayer)( JNIEnv* env, jobject obj, jobject bmp) {
+    if (player != NULL)
+        return -1;
+
+    //access = new HttpAccess();
+    access = new FileAccess("/sdcard/demo.flv");
+    demux = new FFDemux("live.flv");
+    //vout = new TeaVideoOutput();
+    vout = new BmpVideoOutput(env, bmp);
+    player = new TeaPlayer(access, demux, vout);
+    
+    player->Play();
+
+    return 0;
+}
+
+
+JNIEXPORT jint JNICALL JNIDEFINE(renderBMP)(JNIEnv* env, jobject obj, jobject bmp) {
+    if (player != NULL) {
+        vout->BitBlt(env, bmp);
+        return 0;        
+    }
+    return -1;
+}
+
+JNIEXPORT jint JNICALL JNIDEFINE(stopPlayer)( JNIEnv* env , jclass clz) {
+    if ( player == NULL) {
+        player->Stop();
+
+        delete player;
+        delete access;
+        delete demux;
+        delete vout;
+    }
+
+    player = NULL;
+
+    return 0;
+}
+
---
+++ trunk/teaplayer/jni/app_build_android.mk
@@ -0,0 +1,12 @@
+LOCAL_SRC_FILES := framework/vout.cpp\
+                   framework/decode.cpp\
+                   framework/player.cpp\
+                   app/main_jni.cpp\
+                   app/fileaccess.cpp\
+                   app/httpaccess.cpp\
+                   app/ffdemux.cpp\
+                   app/bmp_vout.cpp\
+                   app/yuv420rgb565c.c\
+                   app/yuv2rgb16tab.c
+
+
