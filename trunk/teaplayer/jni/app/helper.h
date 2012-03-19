53a3e67012c3cd1053cc555eb981ff7c84345fe8
achang.zhou@gmail.com <achang.zhou@gmail.com@baff9159-4c17-4706-d244-21966b7d52bd>
2012-03-19 08:52:54 +0000

    Clean code and backup.

--- trunk/teaplayer/jni/app/helper.h
+++ trunk/teaplayer/jni/app/helper.h
@@ -1,9 +1,11 @@
 #ifndef _H_HELPER_H_
 #define _H_HELPER_H_
 
+#ifdef ANDROID
 #include <jni.h>
 #include <android/log.h>
 #define  LOG_TAG    "TEAONLY"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)  
+#endif
 
 #endif
